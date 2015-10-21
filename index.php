9028ce10b3b9275a60da5e4bd0442030fd456924
Matheus Ferreira <matheus.ferreirraa@gmail.com>
2015-10-21 13:31:54 -0200

    Master: primeiro commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+// load composer 
+$loader = require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Application();
+
+$cervejas = array(
+    'marcas' => array('Heineken','Stela','Brahma','Skol'),
+    'estilos' => array('Pilsen','Stout'),
+);
+
+$app->get('/cervejas/{id}', function ($id) use ($cervejas) {
+
+    if ($id == null) {
+        $result = implode(',', $cervejas['marcas']);
+
+        return new Response(json_encode($result), 200);
+    }
+
+    $key = array_key_exists($id, $cervejas['marcas']);
+
+    if ($key === false) {
+        return new Response(json_encode('Ceveja cabo'), 404);
+    }
+
+    return new Response(json_encode($cervejas['marcas'][$id]), 200);
+
+})->value('id', null);
+
+$app->get('/estilos', function () use ($cervejas) {
+    return implode(',', $cervejas['estilos']);
+});
+
+$app->error(function (\Exception $e, $code) {
+     if ($code == '404') {
+         return 'Página não encontrada';
+     }
+
+     return $e->getMessage();
+});
+
+$app->before(function (Request $request) use ($app) {
+    if (!$request->headers->has('authorization')) {
+        return new Response('Unauthorized', 401);
+    }
+
+    require_once 'configs/clientes.php';
+    if (!in_array($request->headers->get('authorization'), array_keys($clientes))) {
+        return new Response('Unauthorized', 401);
+    }
+
+});
+
+$app->after(function (Request $request, Response $response) {
+    $response->headers->set('Content-Type', 'text/json');
+});
+
+$app->run();
